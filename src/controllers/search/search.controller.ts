03ed275143c5fab8c567d6b13bcd25b78cb62d9e
mahmoudamrr <mahmoud.mansour.amr.mm@gmail.com>
2021-02-18 23:35:44 +0200

    search-end

---
+++ src/controllers/search/search.controller.ts
@@ -0,0 +1,47 @@
+import {repository} from '@loopback/repository';
+import {
+  get,
+  param,
+
+
+
+
+  response
+} from '@loopback/rest';
+import {BrandRepository, CategoryRepository, ProductRepository} from '../../repositories';
+
+
+export class SearchController {
+  constructor(
+    @repository(BrandRepository)
+    public brandRepository: BrandRepository,
+    @repository(ProductRepository)
+    public ProductRepository: ProductRepository,
+    @repository(CategoryRepository)
+    public CategoryRepository: CategoryRepository
+  ) { }
+
+
+
+  @get('/search/{search}')
+  @response(200, {
+    description: 'Array of Brand model instances',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'object'
+        },
+      },
+    },
+  })
+  async find(
+     @param.path.string('search') search: string,
+  ) {
+
+    const brandData = await this.brandRepository.find({ where: {or: [{title:{regexp: '[a-z]' || '[A-Z]'} }, {slug: {regexp: '[a-z]' || '[A-Z]'}}]}});
+    const productData = await this.ProductRepository.find({ where:  {or: [{title:{regexp: '[a-z]' || '[A-Z]'} }, {slug: {regexp: '[a-z]' || '[A-Z]'}}]}});
+    const subCateogryData = await this.CategoryRepository.find({ where: {or: [{title:{regexp: '[a-z]' || '[A-Z]'} }, {slug: {regexp: '[a-z]' || '[A-Z]'}}]}});
+    console.log(BrandRepository, ProductRepository, CategoryRepository)
+    return {"brands": brandData, productData, subCateogryData}
+  }
+}
